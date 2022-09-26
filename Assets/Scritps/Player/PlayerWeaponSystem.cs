8807a492cccd262aa1ffdd3a8f71e75adf29ce1a
Agustin <agustin_allende@hotmail.com>
2022-09-26 13:01:56 +0200

    seleccion de armas

--- Assets/Scritps/Player/PlayerWeaponSystem.cs
+++ Assets/Scritps/Player/PlayerWeaponSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerWeaponSystem : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class PlayerWeaponSystem : MonoBehaviour
     private float timeToShoot;
 
     private Dictionary<WeaponType, WeaponObject> weaponObjects = new Dictionary<WeaponType, WeaponObject>();
+    private Dictionary<WeaponType, Image> weaponObjectsImage = new Dictionary<WeaponType, Image>();
     private InputAction shootAction;
 
     private void Awake()
@@ -29,15 +31,23 @@ public class PlayerWeaponSystem : MonoBehaviour
         weaponObjects.Add(WeaponType.Pistol, GetComponent<WeaponPistol>());
         weaponObjects.Add(WeaponType.Shotgun, GetComponent<WeaponShotgun>());
 
+
+        weaponObjectsImage.Add(WeaponType.Sword, GameObject.Find("weapon_1").GetComponent<Image>());
+        weaponObjectsImage.Add(WeaponType.Pistol, GameObject.Find("weapon_2").GetComponent<Image>());
+        weaponObjectsImage.Add(WeaponType.Shotgun, GameObject.Find("weapon_3").GetComponent<Image>());
+
         foreach (var weapon in weaponObjects.Values)
         {
             weapon.enabled = false;
         }
+
         playerControls.Weapons.SwitchTo1.performed += (InputAction.CallbackContext context) => SwitchWeapon(WeaponType.Sword);
         playerControls.Weapons.SwitchTo2.performed += (InputAction.CallbackContext context) => SwitchWeapon(WeaponType.Pistol);
         playerControls.Weapons.SwitchTo3.performed += (InputAction.CallbackContext context) => SwitchWeapon(WeaponType.Shotgun);
 
         shootAction = playerControls.Player.Shoot;
+
+        SwitchWeapon(WeaponType.Sword);
 
     }
 
@@ -46,10 +56,22 @@ public class PlayerWeaponSystem : MonoBehaviour
         if (!weaponObjects.TryGetValue(weaponType, out selectedWeapon))
             return;
 
+        Image weaponImage;
+        if (!weaponObjectsImage.TryGetValue(weaponType, out weaponImage))
+            return;
+
         foreach (var weaponScrip in weaponObjects.Values)
         {
             weaponScrip.enabled = false;
         }
+
+        foreach (var image in weaponObjectsImage.Values)
+        {
+            image.color = Color.black;
+        }
+
+        weaponImage.color = Color.white;    
+
 
         selectedWeapon.enabled = true;
         shootCooldown = selectedWeapon.fireRate / 2;
